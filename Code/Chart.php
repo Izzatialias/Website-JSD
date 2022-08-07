d818bf5e0b892e1f9d8880f91b34ecfd8b351dd0
Izzatialias <102937089+Izzatialias@users.noreply.github.com>
2022-08-07 11:11:04 +0800

    Add files via upload

---
+++ Code/Chart.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link rel="icon" href="https://jsd-chatbot-app.herokuapp.com/UMK-Chatbot.php/iconUMK.png"type="iconUMK.png" sizes ="16x16">
+  <title>JSD CHATBOT</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="HandheldFriendly" content="true"> 
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="{{ url_for('static', filename='styles/style.css') }}">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js"  ></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="CSS/CSS-body2.css">
+  <link rel="stylesheet" type="text/css" href="CSS/CSSmainpage.css">
+  <link rel="stylesheet" href="CSS/bootstrap.min.css"/>
+ 
+<style>
+	.section{
+		height:800px;
+	}
+	
+</style> 
+
+</head>
+ <?php include ("header.php") ?>
+ <?php include ("menu.php") ?><br><br>
+ 
+<body>
+<!--Our Courses-->
+
+<div class="section">
+	<center><h1>Organization Chart</h1><hr>
+	<center><img src="images/chart.png" style="max-height: 700px;" alt="" ></center>
+		
+</div>
+</body>
+<!---footer section--->
+<?php include ("footer.php") ?>
+
+</html>
---
+++ Code/section.php
@@ -0,0 +1,88 @@
+<div class="first"><br><br><br>
+		<div class="container services">
+			<div class="section-title text-center">
+				<h3><!--a href="research@aibig.php" style="color: black" title="Go to Research @ AIBIG"-->OUR EXPERTISE</h3>
+				<p>We provides the opportunity to prepare the new technology</p>
+			</div><hr>
+			<div class="row">
+				<div class="col-md-4 col-sm-4">
+					<div class="service-icon">
+						<img src="images/IoT.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4><a href="research@aibig.php#Internet of Things" style="color: black" title="View research activities in Internet of Things (IoT)">Internet of Things (IoT)</a></h4>
+						<p>System of interrelated computing devices, mechanical and digital machines, objects, animals or people that are provided with unique identifiers and the ability to transfer data over a network without requiring human-to-human or human-to-computer interaction.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="service-icon">
+						<img src="images/AI.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4><a href="research@aibig.php#Artificial Intelligence" style="color: black" title="View research activities in Artificial Intelligence">Artificial Intelligence</a></h4>
+						<p>The ability of a digital computer or computer-controlled robot to perform tasks commonly asssociated with intelligent beings.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4" >
+					<div class="service-icon">
+						<img src="images/ML.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4><a href="research@aibig.php#Machine Learning" style="color: black" title="View research activities in Machine Learning">Machine Learning</a></h4>
+						<p>Scientific study of algorithms and statistical models that computer systems use to perform a specific task without using explicit instructions, but only relying on patterns and inference.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="service-icon">
+						<img src="images/BD.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4><a href="research@aibig.php#Big Data" style="color: black" title="View research activities in Big Data">Big Data</a></h4>
+						<p>Large volume of data – both structured and unstructured – inundates a business on a day-to-day basis.</p>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4">
+					<div class="service-icon">
+						<img src="images/Ds.png" alt="1">
+					</div>
+					<div class="service-content">
+						<h4><a href="research@aibig.php#Data Science" style="color: black" title="View research activities in Data Science">Data Science</a></h4>
+						<p>Multi-disciplinary field that uses scientific methods, processes, algorithms and systems to extract knowledge and insights from structured and unstructured data.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+</div><br><br><br><br>
+<div class="enrollment" style="background-image:url(images/bg.jpg); background-size: 1700px 700px; background-repeat:no-repeat;">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5">
+					<div class="section-title text-white">
+						<h3>ENROLLMENT</h3>
+						<p>How to enroll into our</p>
+					</div>
+					<div class="enroll-list text-white">
+						<div class="enroll-list-item">
+							<span>1</span>
+							<h5>Fill the Form</h5>
+							<p>Please refer to this page how to apply into our program. Click Here</p>
+						</div>
+						<div class="enroll-list-item">
+							<span>2</span>
+							<h5>Get in Touch</h5>
+							<p>Our Administration will contact you when your applications being approved. 
+							If you have any questions, feel free to contact us. Click here</p>
+						</div>
+						<div class="enroll-list-item">
+							<span>3</span>
+							<h5>Register</h5>
+							<p>Come and Register to our faculty. You can refer our offer letter for more information.</p>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+</div><br><br>
+
+
